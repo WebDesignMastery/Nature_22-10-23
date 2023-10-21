ce4900efa849fb09a88fadff4ad0fd8aba433891
Web Design Mastery <130627020+WebDesignMastery@users.noreply.github.com>
2023-10-21 21:03:32 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,41 @@
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+const menuBtnIcon = menuBtn.querySelector("i");
+
+menuBtn.addEventListener("click", (e) => {
+  navLinks.classList.toggle("open");
+
+  const isOpen = navLinks.classList.contains("open");
+  menuBtnIcon.setAttribute(
+    "class",
+    isOpen ? "ri-close-line" : "ri-menu-3-line"
+  );
+});
+
+navLinks.addEventListener("click", (e) => {
+  navLinks.classList.remove("open");
+  menuBtnIcon.setAttribute("class", "ri-menu-3-line");
+});
+
+const scrollRevealOption = {
+  distance: "50px",
+  origin: "bottom",
+  duration: 1000,
+};
+
+// container
+ScrollReveal().reveal(".container__content h1", {
+  ...scrollRevealOption,
+});
+ScrollReveal().reveal(".container__content h2", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+ScrollReveal().reveal(".container__content p", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+ScrollReveal().reveal(".socials", {
+  ...scrollRevealOption,
+  delay: 1500,
+});
